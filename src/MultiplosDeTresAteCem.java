5cfa93bf6fab2b4b93bc280abe92b1c75d79c61b
Kevin Paes <kevin.paes@cvccorp.com.br>
2021-04-12 09:31:39 -0300

    Revert "primeiro commit"
    This reverts commit c72cb32647980cb7dc84b68e21690c4c437ffa55.

--- src/MultiplosDeTresAteCem.java
+++
@@ -1,9 +0,0 @@
-public class MultiplosDeTresAteCem {
-    public static void main(String[] args) {
-        for (int multiplo = 1; multiplo < 100; multiplo++ ){
-            if (multiplo % 3 == 0)    {
-                System.out.println(multiplo);
-            }
-        }
-    }
-}
